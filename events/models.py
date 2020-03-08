55776517600acaefbcbab5f6fc3ad599593b65cd
Darius <Darius.klein@student.hu.nl>
2020-03-08 17:38:46 +0100

    read/write met database en frontend voor testing

--- events/models.py
+++ events/models.py
@@ -2,12 +2,6 @@ from django.db import models
 
 
 class Event (models.Model):
-    #title = models.TextField()
-    #naam = models.TextField()
-    #beschrijving = models.TextField()
-    #begin_datum = models.TextField()
-    #eind_datum = models.TextField()
-
     title = models.CharField(max_length=50)
     naam = models.CharField(max_length=50)
     beschrijving = models.TextField()
--- events/views.py
+++ events/views.py
@@ -8,4 +8,10 @@ from events.models import Event
 def index(response, url_id):
     print(url_id)
     event = Event.objects.get(id=url_id)
-    return HttpResponse("<h1>%s</h1>" %event.naam)
+    html1 = "<h1>%s</h1>"  % event.title + \
+            "<html><body>Test page <br> </body></html>" + \
+            "<html><body>Naam: %s <br> </body></html>" % event.naam + \
+            "<html><body>Beschrijving: %s <br> </body></html>" % event.beschrijving + \
+            "<html><body>Begin Datum: %s <br> </body></html>" % event.begin_datum + \
+            "<html><body>Eind Datum: %s <br> </body></html>" % event.eind_datum
+    return HttpResponse(html1)
